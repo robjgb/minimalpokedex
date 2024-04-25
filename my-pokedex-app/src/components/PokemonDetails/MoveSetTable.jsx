8b81fc285d8a233df6fe628e0e8f0dd2b7e664ce
Robert Balatbat <73982521+robjgb@users.noreply.github.com>
2024-04-24 17:55:12 -0700

    Add sorting + icons to moveset table

--- my-pokedex-app/src/components/PokemonDetails/MoveSetTable.jsx
+++ my-pokedex-app/src/components/PokemonDetails/MoveSetTable.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
+import physicalIcon from '../../assets/move-physical.png';
+import specialIcon from '../../assets/move-special.png';
+import statusIcon from '../../assets/move-status.png';
+import { ArrowsUpDownIcon, BarsArrowUpIcon, BarsArrowDownIcon } from '@heroicons/react/24/outline';
 
 const MovesetLoader = () => {
     return (
         <div className="overflow-x-auto rounded-lg border border-gray-200">
-            <table className="min-w-full divide-y-2 bg-white text-sm border-separate border-spacing-2 ">
-                <thead className="ltr:text-left rtl:text-right">
+            <table className="min-w-full divide-y-2 text-sm border-separate border-spacing-2 bg-gray-100">
+                <thead className="text-center">
                     <tr>
-                        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">#</th>
-                        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Name</th>
-                        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Type</th>
-                        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Cat.</th>
-                        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Power</th>
-                        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Acc.</th>
-                        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">PP</th>
+                        <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">#</th>
+                        <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">Name</th>
+                        <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">Type</th>
+                        <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">Cat.</th>
+                        <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">Power</th>
+                        <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">Acc.</th>
+                        <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">PP</th>
                     </tr>
                 </thead>
                 <tbody className="">
@@ -34,6 +38,38 @@ const MovesetLoader = () => {
     )
 }
 
+const TableHeader = ({ column, sortConfig, onSort }) => {
+    const { key, label } = column;
+    const { key: sortKey, direction: sortDirection, status: sortStatus } = sortConfig;
+
+    const isActive = sortKey === key;
+    const iconClass = 'w-4 h-4 ml-1';
+
+    let icon;
+    if (isActive) {
+        if (sortStatus === 'asc') {
+            icon = <BarsArrowUpIcon className={`${iconClass} text-black`} />;
+        } else if (sortStatus === 'desc') {
+            icon = <BarsArrowDownIcon className={`${iconClass} text-black`} />;
+        } else {
+            icon = <ArrowsUpDownIcon className={`${iconClass} text-gray-400`} />;
+        }
+    } else {
+        icon = <ArrowsUpDownIcon className={`${iconClass} text-gray-400`} />;
+    }
+
+    return (
+        <th
+            className="whitespace-nowrap px-4 py-2 font-bold text-gray-900 cursor-pointer"
+            onClick={() => onSort(key)}
+        >
+            <div className="flex items-center justify-center">
+                {label}
+                {icon}
+            </div>
+        </th>
+    );
+};
 
 export default function MovesetTable({ moves, versionGroups, selectedVersionGroup, onVersionGroupChange }) {
     const [moveData, setMoveData] = useState({});
@@ -41,12 +77,112 @@ export default function MovesetTable({ moves, versionGroups, selectedVersionGrou
     const [moveLearnMethods, setMoveLearnMethods] = useState([]);
     const [machineData, setMachineData] = useState({});
     const [filteredMoves, setFilteredMoves] = useState([]);
+    const [sortConfig, setSortConfig] = useState({ key: null, direction: '', status: 'default' });
+    const [sortedMoves, setSortedMoves] = useState([]);
+
+    const columns = [
+        { key: '#', label: '#' },
+        { key: 'name', label: 'Name' },
+        { key: 'type', label: 'Type' },
+        { key: 'cat.', label: 'Cat.' },
+        { key: 'power', label: 'Power' },
+        { key: 'acc.', label: 'Acc.' },
+        { key: 'pp', label: 'PP' },
+    ];
+
+    const damageClassIcons = {
+        physical: physicalIcon,
+        special: specialIcon,
+        status: statusIcon,
+    };
+
+    const getLearnMethodValue = (details, moveName) => {
+        const { move_learn_method } = details;
+        const methodName = move_learn_method.name;
+
+        if (methodName === 'level-up') {
+            return 0;
+        } else if (methodName === 'machine') {
+            const machineNumber = machineData[moveName]?.match(/\d+/)?.[0] || 0;
+            return methodName === 'hm' ? 200 + parseInt(machineNumber, 10) : 100 + parseInt(machineNumber, 10);
+        } else if (methodName === 'egg') {
+            return 300;
+        }
+        return 400;
+    };
+
+    useEffect(() => {
+        const sortMoves = () => {
+            const sorted = [...filteredMoves].sort((a, b) => {
+                switch (sortConfig.key) {
+                    case '#':
+                        const detailsA = a.version_group_details?.find(detail => detail.version_group.name === selectedVersionGroup);
+                        const detailsB = b.version_group_details?.find(detail => detail.version_group.name === selectedVersionGroup);
+                        const learnMethodA = detailsA && getLearnMethodValue(detailsA, a.move.name);
+                        const learnMethodB = detailsB && getLearnMethodValue(detailsB, b.move.name);
+                        if (!detailsA || !detailsB) {
+                            return 0;
+                        }
+        
+                        if (learnMethodA !== learnMethodB) {
+                            return learnMethodA - learnMethodB;
+                        }
+                        return parseInt(detailsA.level_learned_at, 10) - parseInt(detailsB.level_learned_at, 10);
+                    case 'name':
+                        return a.move.name.localeCompare(b.move.name);
+                    case 'type':
+                        return moveData[a.move.name]?.type?.name.localeCompare(moveData[b.move.name]?.type?.name);
+                    case 'cat.':
+                        const categoryOrder = { physical: 0, special: 1, status: 2 };
+                        return categoryOrder[moveData[a.move.name]?.damage_class?.name] - categoryOrder[moveData[b.move.name]?.damage_class?.name];
+                    case 'power':
+                        return (moveData[a.move.name]?.power || 0) - (moveData[b.move.name]?.power || 0);
+                    case 'acc.':
+                        return (moveData[a.move.name]?.accuracy || 0) - (moveData[b.move.name]?.accuracy || 0);
+                    case 'pp':
+                        return (moveData[a.move.name]?.pp || 0) - (moveData[b.move.name]?.pp || 0);
+                    default:
+                        return 0;
+                }
+            });
+    
+            if (sortConfig.status === 'default') {
+                return filteredMoves;
+            } else if (sortConfig.direction === 'desc') {
+                return sorted.reverse();
+            }
+            return sorted;
+        };
+    
+        setSortedMoves(sortMoves());
+    }, [filteredMoves, sortConfig, selectedVersionGroup, moveData]);
+
+
+    const handleSort = (key) => {
+        let direction = 'asc';
+        let status = 'asc';
+
+        if (sortConfig.key === key) {
+            if (sortConfig.status === 'default') {
+                direction = 'asc';
+                status = 'asc';
+            } else if (sortConfig.status === 'asc') {
+                direction = 'desc';
+                status = 'desc';
+            } else {
+                direction = '';
+                status = 'default';
+            }
+        }
+
+        setSortConfig({ key, direction, status });
+    };
 
     const fetchMoveData = async (moveUrl) => {
         const response = await fetch(moveUrl);
         return await response.json();
     };
-    
+
     const fetchMachineData = async (machineUrl) => {
         const response = await fetch(machineUrl);
         const machineInfo = await response.json();
@@ -55,26 +191,27 @@ export default function MovesetTable({ moves, versionGroups, selectedVersionGrou
 
     useEffect(() => {
         const fetchData = async () => {
+            setSortConfig({ key: null, direction: '', status: 'default' });
             setIsLoading(true);
-    
+
             const filteredMovesData = moves.filter(move =>
                 move.version_group_details.some(detail => detail.version_group.name === selectedVersionGroup)
             );
-    
+
             const moveDataPromises = filteredMovesData.map(move => fetchMoveData(move.move.url));
             const moveDataArray = await Promise.all(moveDataPromises);
-    
+
             const moveDataObject = moveDataArray.reduce((acc, data, index) => {
                 acc[filteredMovesData[index].move.name] = data;
                 return acc;
             }, {});
-    
+
             const machineDataPromises = filteredMovesData.map(move => {
                 const details = move.version_group_details.find(
                     detail => detail.version_group.name === selectedVersionGroup
                 );
                 const data = moveDataObject[move.move.name];
-    
+
                 if (details.move_learn_method.name === 'machine') {
                     const machine = data?.machines?.find(
                         machine => machine.version_group.name === selectedVersionGroup
@@ -85,22 +222,22 @@ export default function MovesetTable({ moves, versionGroups, selectedVersionGrou
                 }
                 return Promise.resolve(null);
             });
-    
+
             const machineDataArray = await Promise.all(machineDataPromises);
-    
+
             const machineDataObject = machineDataArray.reduce((acc, data, index) => {
                 if (data) {
                     acc[filteredMovesData[index].move.name] = data;
                 }
                 return acc;
             }, {});
-    
+
             setFilteredMoves(filteredMovesData);
             setMoveData(moveDataObject);
             setMachineData(machineDataObject);
             setIsLoading(false);
         };
-    
+
         fetchData();
     }, [moves, selectedVersionGroup]);
 
@@ -112,7 +249,7 @@ export default function MovesetTable({ moves, versionGroups, selectedVersionGrou
         if (methodName === 'level-up') {
             return `lv. ${details.level_learned_at}`;
         } else if (methodName === 'machine') {
-            return machineData[moveName]|| 'tm/hm';
+            return machineData[moveName] || 'tm/hm';
         } else {
             const learnMethod = moveLearnMethods.find((method) => method.name === methodName);
             return learnMethod ? learnMethod.name : methodName;
@@ -182,19 +319,20 @@ export default function MovesetTable({ moves, versionGroups, selectedVersionGrou
             ) : (
                 <div className="overflow-x-auto rounded-lg border border-gray-200">
                     <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-                        <thead className="text-center">
-                            <tr className=''>
-                                <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">#</th>
-                                <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">name</th>
-                                <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">type</th>
-                                <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">cat.</th>
-                                <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">power</th>
-                                <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">acc.</th>
-                                <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">pp</th>
+                        <thead className="text-center bg-gray-100">
+                            <tr>
+                                {columns.map((column) => (
+                                    <TableHeader
+                                        key={column.key}
+                                        column={column}
+                                        sortConfig={sortConfig}
+                                        onSort={handleSort}
+                                    />
+                                ))}
                             </tr>
                         </thead>
                         <tbody className="divide-y divide-gray-200 text-center">
-                            {filteredMoves.map(move => {
+                            {sortedMoves.map(move => {
                                 const details = move.version_group_details.find(
                                     detail => detail.version_group.name === selectedVersionGroup
                                 );
@@ -209,7 +347,15 @@ export default function MovesetTable({ moves, versionGroups, selectedVersionGrou
                                             {data?.type?.name || '-'}
                                         </td>
                                         <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                                            {data?.damage_class?.name || '-'}
+                                            {data?.damage_class?.name ? (
+                                                <img
+                                                    src={damageClassIcons[data.damage_class.name]}
+                                                    alt={data.damage_class.name}
+                                                    className="w-6 h-4 mx-auto"
+                                                />
+                                            ) : (
+                                                '-'
+                                            )}
                                         </td>
                                         <td className="whitespace-nowrap px-4 py-2 text-gray-700">
                                             {data?.power || '-'}
